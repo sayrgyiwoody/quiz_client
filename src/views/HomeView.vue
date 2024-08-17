180237d146b1476aecbcb29db7a23a3cff5694f6
Woody <waiyanwoody@gmail.com>
2024-08-17 14:31:42 +0630

    bruh d tine lyy bl kg pr dl

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,7 +11,7 @@
             </div>
             <div @click="directAll" class="h-fit py-4 cursor-pointer animate__animated animate__bounceIn font-medium hover:bg-slate-50 dark:hover:bg-zinc-700 duration-150 p-3 md:w-2/6 text-zinc-900 dark:text-white bg-white shadow dark:bg-zinc-800 flex items-center justify-center md:ms-3 rounded px-3 dark:border border-zinc-800 border-0 dark:border-zinc-700">
                 <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M174 232a6 6 0 0 1-6 6H88a6 6 0 0 1 0-12h80a6 6 0 0 1 6 6Zm40-128a85.56 85.56 0 0 1-32.88 67.64A18.23 18.23 0 0 0 174 186v6a14 14 0 0 1-14 14H96a14 14 0 0 1-14-14v-6a18 18 0 0 0-7-14.23a85.59 85.59 0 0 1-33-67.24C41.74 57.91 79.39 19.12 125.93 18A86 86 0 0 1 214 104Zm-12 0a74 74 0 0 0-75.79-74C86.17 31 53.78 64.34 54 104.42a73.67 73.67 0 0 0 28.4 57.87A29.92 29.92 0 0 1 94 186v6a2 2 0 0 0 2 2h64a2 2 0 0 0 2-2v-6a30.18 30.18 0 0 1 11.7-23.78A73.59 73.59 0 0 0 202 104Zm-20.08-9A55.58 55.58 0 0 0 137 50.08a6 6 0 1 0-2 11.84c17.38 2.92 32.13 17.68 35.08 35.08a6 6 0 0 0 5.91 5a6.87 6.87 0 0 0 1-.08a6 6 0 0 0 4.93-6.92Z"/></svg>
-                <span class="ms-2 text-xs">All Quizzes</span>
+                <span class="ms-2 text-xs md:text-base">All Quizzes</span>
             </div>
             <!-- <div @click="enterRoom" class="h-fit py-4 cursor-pointer animate__animated animate__bounceIn font-medium hover:bg-slate-50 dark:hover:bg-zinc-700 duration-150 p-3 ms-2 md:w-2/6 text-zinc-900 dark:text-white bg-white shadow dark:bg-zinc-800 flex items-center justify-center md:ms-3 rounded px-3 dark:border border-zinc-800 border-0 dark:border-zinc-700">
                 <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 256 256"><path fill="currentColor" d="M24 168V56a16 16 0 0 1 16-16h176a16 16 0 0 1 16 16v112a8 8 0 0 1-16 0V56H40v112a8 8 0 0 1-16 0Zm224 32a8 8 0 0 1-8 8H16a8 8 0 0 1 0-16h96v-24a8 8 0 0 1 8-8h64a8 8 0 0 1 8 8v24h48a8 8 0 0 1 8 8Zm-120-8h48v-16h-48Z"/></svg>
@@ -23,7 +23,7 @@
         </div>
 
         <div v-if="searchKey" class="mx-auto max-w-5xl mt-4">
-            <div class="flex  justify-between items-center text-zinc-800 dark:text-slate-200 mb-3 text-xl">
+            <div class="flex  justify-between items-center text-zinc-800 dark:text-slate-200 mb-3 md:text-xl">
                 <span class="flex items-center hover:text-primary duration-100 font-semibold">
                     <svg class=" inline-block me-2" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 24 24"><path fill="currentColor" d="M6 9H3c-.55 0-1-.45-1-1s.45-1 1-1h3c.55 0 1 .45 1 1s-.45 1-1 1zm0 3H3c-.55 0-1 .45-1 1s.45 1 1 1h3c.55 0 1-.45 1-1s-.45-1-1-1zm13.88 6.29l-3.12-3.12c-.86.56-1.89.88-3 .82c-2.37-.11-4.4-1.96-4.72-4.31a5.013 5.013 0 0 1 5.83-5.61c1.95.33 3.57 1.85 4 3.78c.33 1.46.01 2.82-.7 3.9l3.13 3.13c.39.39.39 1.02 0 1.41c-.39.39-1.03.39-1.42 0zM17 11c0-1.65-1.35-3-3-3s-3 1.35-3 3s1.35 3 3 3s3-1.35 3-3zM3 19h8c.55 0 1-.45 1-1s-.45-1-1-1H3c-.55 0-1 .45-1 1s.45 1 1 1z"/></svg>
                     Searched : <span class="text-primary ms-2">{{searchKey}}</span></span>
